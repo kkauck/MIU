8c1f6449a9a3baa2d7fe613169123979e5e513d9
g78624 <kyle0nlin3@fullsail.edu>
2013-05-19 15:57:17 -0600

    Minor updates to HTML, Major to JS
    May 19, 2013

--- kauck_kyle_project3/gold_app/js/main.js
+++ kauck_kyle_project3/gold_app/js/main.js
@@ -1,9 +1,9 @@
 $("#index").on("pageinit", function(){
                 
-    function idGrabber(y) {
+    /*function idGrabber(y) {
         var idTag = document.getElementById(y);
         return idTag;
-    };
+    };*/
 
     //Creates all my Xbox Data
     function xboxFetch(){
@@ -434,6 +434,7 @@ $("#addItem").on("pageinit", function(){
                 submitHandler: function() {
 		    var data = addGameForm.serializeArray();
 		    storeData(data);
+                    window.location.reload();
                 }
 		
 	});
@@ -445,22 +446,191 @@ $("#addItem").on("pageinit", function(){
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-var autofillData = function (){
+
+/*Edit Function not yet working correctly!
+var editMyGame = function (){
 	 
-};
+	    var gameInformation = localStorage.getItem(this.key);
+	    var gameLibrary = JSON.parse(gameInformation);
+	    
+	    
+	    $("#gameTitle").val(gameLibrary.gameTitle[1]);
+	    $("#consoles").val(gameLibrary.console[1]);
+	    $("#genre").val(gameLibrary.genre[1]);
+	    $("#dateAdded").val(gameLibrary.dateAdded[1]);
+	    $("#rating").val(gameLibrary.rating[1]);
+            $("input[name=multiplayer]:checked").val(gameLibrary.multiplayer[1]);
+	    var multiplayer = document.getElementById("addGameForm").multiplayer;
+        for (var c = 0, d = multiplayer.length; c < d; c++) {
+	        if (multiplayer[c].value == "Yes" && gameLibrary.multiplayer[1] == "Yes") {
+	        	multiplayer[c].setAttribute("checked", "checked");
+	        } else if (multiplayer[c].value == "No" && gameLibrary.multiplayer[1] == "No") {
+	        	multiplayer[c].setAttribute("checked", "checked");    
+	        }
+        }
+	    $("#downloadSize").val(gameLibrary.download[1]);
+	    $("#additionalInfo").val(gameLibrary.additional[1]);
+	    
+	    saveInfo.removeEventListener("click", dataStorage);
+	    $("#submitButton").val("Edit Game");
+	    var gameEditor = $("#submitButton");
+	    gameEditor.addEventListener("click", fieldCheck);
+	    gameEditor.key = this.key;
+         
+};*/
 
+
+//This will display all of the data that is inside of local storage
 var getData = function(){
-
+                
+    	if(localStorage.length === 0){
+	    	alert("You currently have no data in local storage, game data will be automatically added");
+	    	addGameData();
+    	}
+		var createInfo = document.getElementById("gameInfoDisplay");
+		for (var e = 0, f = localStorage.length; e < f; e++) {
+		    var gameKey = localStorage.key(e);
+		    var gameValue = localStorage.getItem(gameKey);
+		    var gameInfoObject = JSON.parse(gameValue);
+            var gameList = document.createElement("p");
+			gameList.setAttribute("id", "gameListInfo");
+			createInfo.appendChild(gameList);
+			var listButtons = document.createElement("span");
+			listButtons.setAttribute("id", "listButtons");
+			imageAddition(gameInfoObject.console[1], gameList);
+            for (var g in gameInfoObject) {
+				var createGameList = document.createElement("span");
+                createGameList.setAttribute("id", "gameInfoList");
+				gameList.appendChild(createGameList);
+				var gameTextOutput = gameInfoObject[g][0] + gameInfoObject[g][1];
+				createGameList.innerHTML = gameTextOutput;
+				gameList.appendChild(listButtons)
+            }
+            createButtons(localStorage.key(e), listButtons);
+		}
+                
 };
 
-var storeData = function(data){
-	
-}; 
 
-var deleteItem = function (){
+//This will store all data from the form into Local Storage
+var storeData = function(data, gameKey){
+    
+    if (!gameKey) {
+        	var idGenerator = Math.floor(Math.random()*1000000000);
+        } else {
+	    	idGenerator = gameKey;    
+        }
+                
+       var idGenerator = Math.floor(Math.random()*1000000000);
+        var multiplayerSelection = $("input[name=multiplayer]:checked").val();
+        var gameLibrary = {
+            gameTitle  :["", $("#gameTitle").val()],
+            console    :["<strong>Console:</strong> ", $("#consoles").val()],
+            genre      :["<strong>Genre:</strong> ", $("#genre").val()],
+            dateAdded  :["<strong>Date Added:</strong> ", $("#dateAdded").val()],
+            rating     :["<strong>Rating:</strong> ", $("#rating").val()],
+            multiplayer:["<strong>Multiplayer Functionality:</strong> ", multiplayerSelection],
+            download   :["<strong>Download Size:</strong> ", $("#downloadSize").val()],
+            additional :["<strong>Additional Information:</strong> ", $("#additionalInfo").val()]
+        };
+        localStorage.setItem(idGenerator, JSON.stringify(gameLibrary));
+        alert("Game Information Has Been Saved!");
+};
+
+
+//This will ask the user if they would like to delete a single game.
+    var deleteMyGame = function (){
+                
+    var confirmDelete = confirm("Please confirm that you would like to delete this game.");
+	    if (confirmDelete) {
+		    localStorage.removeItem(this.key)
+		    alert("This game was successfully deleted from storage.")
+	    } else {
+		    alert("Your game was not deleted.");
+	    }
 			
 };
-					
-var clearLocal = function(){
-
+		
+//This will delete all of the local storage that is currently saved			
+    var clearLocal = function(){
+                
+	    if(localStorage.length === 0){
+		alert("You have no saved data to delete.");    
+	    } else {
+		localStorage.clear();
+		alert("You have successfully cleared your data");
+		window.location.reload();
+		return false;
+	    }
 };
+
+    //Creates my buttons to delete and edit single games
+    function createButtons(gameKey, listButtons){
+	   /* Removed due to not working yet
+            var createEditButton = document.createElement("a");
+	    createEditButton.setAttribute("id", "createdButton");
+	    createEditButton.href = "#addItem";
+	    createEditButton.key = gameKey;
+	    createEditButton.innerHTML = "Edit Game";
+	    createEditButton.addEventListener("click", editMyGame)
+	    listButtons.appendChild(createEditButton);*/
+	    
+	    var createDeleteButton = document.createElement("a");
+	    createDeleteButton.setAttribute("id", "createdButton");
+	    createDeleteButton.href = "#index";
+	    createDeleteButton.key = gameKey;
+	    createDeleteButton.innerHTML = "Delete Game";
+	    createDeleteButton.addEventListener("click", deleteMyGame);
+	    listButtons.appendChild(createDeleteButton);
+    }
+
+    //This function will add an image into my display
+    //Displays it only for my "Display Item Page"
+    function imageAddition(consoleValue, newLi){
+	    if(consoleValue === "Xbox 360"){
+	    	var imageTag = document.createElement("img");
+	    	imageTag.setAttribute("src", "img/xbox360.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "Playstation 3") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/playstationThree.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "Wii U") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/wiiU.gif");
+	    	newLi.appendChild(imageTag);  
+	    } else if (consoleValue === "PC") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/pc.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "Playstation Vita") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/playstationVita.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "Nintendo 3DS") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/nintendo3ds.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "iPhone/iPad") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/iPhone.gif");
+	    	newLi.appendChild(imageTag);
+	    } else if (consoleValue === "Android") {
+	    	var imageTag = document.createElement("img");
+	    	var imageSource = imageTag.setAttribute("src", "img/android.gif");
+	    	newLi.appendChild(imageTag);
+	    }
+    }
+    
+    //Add game data if none is in local storage!
+    function addGameData(){
+	    for (var r in gamingData){
+		    var idGenerator = Math.floor(Math.random()*1000000000);
+		    localStorage.setItem(idGenerator, JSON.stringify(gamingData[r]));
+	    }
+    }
+
+var clearData 	= $("#clearData");
+    clearData.click(clearLocal);
+var displayData = $("#displayData");
+    displayData.click(getData);
